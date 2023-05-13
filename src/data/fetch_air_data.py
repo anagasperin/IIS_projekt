0bb77ab2beae55930cd08ff8dba45d85436b7dd7
anagasperin <ana.gasperin@student.um.si>
2023-05-13 15:54:07 +0200

    initial commit

---
+++ src/data/fetch_air_data.py
@@ -0,0 +1,39 @@
+import json
+import os
+import pandas as pd
+import requests
+import time
+from datetime import datetime
+
+root_dir = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '../..'))
+save_path = os.path.join(root_dir, 'data', 'raw')
+
+
+    # print('Fetching air data...')
+    # url = "https://api.modra.ninja/jcdecaux/maribor/stations"
+    # response = requests.get(url)
+    # if response.status_code == 200:
+    #     print("Fetched main datset")
+    #     data = json.loads(response.content)
+    #     with open(air, "w") as f:
+    #         json.dump(data, f)
+    # else:
+    #     print("Failed to retrieve JSON data")
+
+while True:
+    # Fetch the data from the API
+    response = requests.get("https://api.modra.ninja/jcdecaux/maribor/stations")
+    data = response.json()
+
+    # Convert the data to a DataFrame
+    df = pd.json_normalize(data)
+
+    # Create a filename based on the current datetime
+    filename = os.path.join(save_path, datetime.now().strftime("%Y-%m-%d_%H-%M-%S.csv"))
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(filename, index=False)
+
+    # Wait for an hour before repeating the process
+    time.sleep(3600)
---
+++ src/models/predict_model.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import mlflow
+
+
+def predict_model(train_path, test_path, model_path, train_metrics_path, metrics_path):
+    MLFLOW_TRACKING_URI = "https://dagshub.com/anagasperin/IIS.mlflow"
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment("mlruns")
+    # mlflow.sklearn.autolog()
+
+    with mlflow.start_run():
+        csv = pd.read_csv(train_path, encoding='utf_8')
+        train = pd.DataFrame(csv)
+
+        csv = pd.read_csv(test_path, encoding='utf_8')
+        test = pd.DataFrame(csv)
+        print('Data read')
+
+        columns = np.array(train.columns)
+        mask = columns != 'pm10'
+        x_train = train[columns[mask]]
+        y_train = train['pm10']
+        x_test = test[columns[mask]]
+        y_test = test['pm10']
+
+        model = LinearRegression()
+        model.fit(x_train, y_train)
+        print('Model trained')
+
+        prediction = model.predict(x_test)
+
+        # Calculate MSE and MAE for the test data
+        mse_test = mean_squared_error(y_test, prediction)
+        mae_test = mean_absolute_error(y_test, prediction)
+        evs_test = explained_variance_score(y_test, prediction)
+
+        mlflow.log_metric("MSE Test", mse_test)
+        mlflow.log_metric("MAE Test", mae_test)
+        mlflow.log_metric("EVS Test", evs_test)
+
+        # Make predictions for the training data
+        predictions_train = model.predict(x_train)
+
+        # Calculate MSE and MAE for the training data
+        mse_train = mean_squared_error(y_train, predictions_train)
+        mae_train = mean_absolute_error(y_train, predictions_train)
+        evs_train = explained_variance_score(y_train, predictions_train)
+
+        mlflow.log_metric("MSE Train", mse_train)
+        mlflow.log_metric("MAE Train", mae_train)
+        mlflow.log_metric("EVS Train", evs_train)
+
+        with open(train_metrics_path, 'w') as file:
+            file.write('MAE:' + str(mae_train) + '\n')
+            file.write('MSE:' + str(mse_train) + '\n')
+            file.write('EVS:' + str(evs_train) + '\n')
+
+        with open(metrics_path, 'w') as file:
+            file.write('MAE:' + str(mae_test) + '\n')
+            file.write('MSE:' + str(mse_test) + '\n')
+            file.write('EVS:' + str(evs_test) + '\n')
+
+        print('Reports updated')
+
+        with open(model_path, 'wb') as f:
+            pickle.dump(model, f)
+
+        print('Model serialized')
+
+    autolog_run = mlflow.last_active_run()
+    print(autolog_run)
+
+
+def main():
+    import os
+
+    root_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '../..'))
+
+    train_path = os.path.join(root_dir, 'data', 'processed', 'train.csv')
+    test_path = os.path.join(root_dir, 'data', 'processed', 'test.csv')
+    model_path = os.path.join(root_dir, 'models', 'linear')
+    train_metrics_path = os.path.join(root_dir, 'reports', 'train_metrics.txt')
+    metrics_path = os.path.join(root_dir, 'reports', 'metrics.txt')
+
+    predict_model(train_path, test_path, model_path,
+                  train_metrics_path, metrics_path)
+
+
+if __name__ == '__main__':
+    main()
