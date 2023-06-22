987d1181d50c36615ac888e3d739d63be4bbcea7
anagasperin <anagasperin99@gmail.com>
2023-06-22 17:05:52 +0200

    merge v2

--- src/data/merge_bike_data.py
+++ src/data/merge_bike_data.py
@@ -1,29 +1,36 @@
-import os
 import csv
+import os
 import json
 
-output_data = []
+# Specify the directory where your CSV files are located
+directory = 'data/raw/'
 
-# Assuming the CSV files are located in the 'data/raw/' directory
-csv_directory = 'data/raw/'
+# Initialize the merged data list
+merged_data = []
 
-for filename in os.listdir(csv_directory):
+# Iterate over the CSV files in the directory
+for filename in os.listdir(directory):
     if filename.endswith('.csv'):
+        file_path = os.path.join(directory, filename)
         file_date = filename.split('.')[0]  # Extract the date from the file name
-        with open(csv_directory + filename, 'r') as csv_file:
-            reader = csv.reader(csv_file)
+
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
             for row in reader:
-                # Assuming each row in the CSV file is a list of values
-                # Modify this logic according to your actual CSV format
+                # Remove the 'type' and 'bbox' keys from the row dictionary
+                row.pop('type', None)
+                row.pop('bbox', None)
+
+                # Create the data entry dictionary
                 data_entry = {
                     'date': file_date,
                     'values': row
                 }
-                output_data.append(data_entry)
+
+                # Append the data entry to the merged data list
+                merged_data.append(data_entry)
 
 # Write the merged data to a JSON file
 output_file = 'data/bike/merged_bike_data.json'
 with open(output_file, 'w') as json_file:
-    json.dump(output_data, json_file)
-
-
+    json.dump(merged_data, json_file)
