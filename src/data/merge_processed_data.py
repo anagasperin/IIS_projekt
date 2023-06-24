8a4c2e697359ff8c5825d7ea98ff630a8218da58
anagasperin <anagasperin99@gmail.com>
2023-06-24 19:37:35 +0200

    merge weather and bike data

--- src/data/merge_processed_data.py
+++ src/data/merge_processed_data.py
@@ -14,19 +14,19 @@ def main():
     weather_df = pd.read_csv(weather_proc)
 
     # bike_df['datetime'] = pd.to_datetime(bike_df['datetime'])
-    bike_df['date'] = pd.to_datetime(bike_df['datetime'])
-    bike_df['hour'] = bike_df['datetime'].dt.hour
+    bike_df['date'] = pd.to_datetime(bike_df['datetime']).dt.date
+    bike_df['hour'] = pd.to_datetime(bike_df['datetime']).dt.hour
 
     # weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
-    weather_df['date'] = pd.to_datetime(weather_df['datetime'])
-    weather_df['hour'] = weather_df['datetime'].dt.hour
+    weather_df['date'] = pd.to_datetime(weather_df['datetime']).dt.date
+    weather_df['hour'] = pd.to_datetime(weather_df['datetime']).dt.hour
 
     # bike_df['hour'] = bike_df['date'].dt.hour
     # weather_df['hour'] = weather_df['date'].dt.hour
 
     merged_data = bike_df.merge(weather_df, on=['date', 'hour'], how='inner')
 
-    merged_data = merged_data.drop(columns=['datetime'])
+    merged_data = merged_data.drop(columns=['datetime_x', 'datetime_y', 'date'])
 
     merged_data.to_csv(output_file, index=False)
 
