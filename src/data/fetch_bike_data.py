974b31602de5c835689baddc0d23bca8f6dbdbcf
Julija <julija.pesticek@student.um.si>
2023-07-01 19:49:24 +0200

    prvaVerzija

--- src/data/fetch_bike_data.py
+++ src/data/fetch_bike_data.py
@@ -5,9 +5,13 @@ import requests
 import time
 from datetime import datetime
 import pytz
-import dvc.api
 
 def main():
+    root_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '../..'))
+    
+    save_path = os.path.join(root_dir, 'data', 'raw')
+
     # Fetch the data from the API
     response = requests.get("https://api.modra.ninja/jcdecaux/maribor/stations")
     data = response.json()
@@ -19,23 +23,10 @@ def main():
     timezone = pytz.timezone('Europe/Berlin')
 
     # Create a filename based on the current datetime
-    filename = datetime.now(timezone).strftime("%Y-%m-%d_%H-%M-%S.csv")
+    filename = os.path.join(save_path, datetime.now(timezone).strftime("%Y-%m-%d_%H-%M-%S.csv"))
 
-    # Define the DVC data directory
-    dvc_data_dir = 'data/raw'
-
-    # Create the raw data directory if it doesn't exist
-    os.makedirs(dvc_data_dir, exist_ok=True)
-
-    # Save the DataFrame to a CSV file in the DVC data directory
-    file_path = os.path.join(dvc_data_dir, filename)
-    df.to_csv(file_path, index=False)
-
-    # Push the file to DVC
-    dvc.api.push(file_paths=[file_path], remote='origin')
-
-    # Remove the temporary file (optional since it's already in DVC)
-    os.remove(file_path)
+    # Save the DataFrame to a CSV file
+    df.to_csv(filename, index=False)
 
 if __name__ == '__main__':
     main()
