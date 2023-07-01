bf2a34c333875de93e2bdc2bf6f943937e338ea6
Julija <julija.pesticek@student.um.si>
2023-07-01 19:40:41 +0200

    tryFetch Bike

--- src/data/fetch_bike_data.py
+++ src/data/fetch_bike_data.py
@@ -5,12 +5,15 @@ import requests
 import time
 from datetime import datetime
 import pytz
+import dvc.api
 
 def main():
-    root_dir = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '../..'))
+    # root_dir = os.path.abspath(os.path.join(
+    #     os.path.dirname(__file__), '../..'))
     
-    save_path = os.path.join(root_dir, 'data', 'raw')
+    # save_path = os.path.join(root_dir, 'data', 'raw')
+
+    
 
     # Fetch the data from the API
     response = requests.get("https://api.modra.ninja/jcdecaux/maribor/stations")
@@ -23,10 +26,19 @@ def main():
     timezone = pytz.timezone('Europe/Berlin')
 
     # Create a filename based on the current datetime
-    filename = os.path.join(save_path, datetime.now(timezone).strftime("%Y-%m-%d_%H-%M-%S.csv"))
+    filename = datetime.now(timezone).strftime("%Y-%m-%d_%H-%M-%S.csv")
+
+    dvc_data_dir = 'data/raw'
+    temp_file = f"{dvc_data_dir}/{filename}"
 
     # Save the DataFrame to a CSV file
     df.to_csv(filename, index=False)
+
+    # Push the temporary file to DVC
+    dvc.api.push(temp_file, remote='origin')
+
+    # Remove the temporary file
+    os.remove(temp_file)
 
 if __name__ == '__main__':
     main()
