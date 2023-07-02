1d4be289272932e56e6d31180c1ba9b1dc11fc69
anagasperin <anagasperin99@gmail.com>
2023-07-02 19:27:16 +0200

    pipeline update

--- src/data/merge_processed_data.py
+++ src/data/merge_processed_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from datetime import datetime
+import tempfile
 
 def main():
     root_dir = os.path.abspath(os.path.join(
@@ -9,24 +10,27 @@ def main():
     bike_proc = os.path.join(root_dir, 'data', 'processed', 'bike_data.csv')
     weather_proc = os.path.join(root_dir, 'data', 'processed', 'weather_data.csv')
     output_file = os.path.join(root_dir, 'data', 'processed', 'current_data.csv')
+    os.makedirs(bike_proc, weather_proc, output_file, exist_ok=True)
 
-    bike_df = pd.read_csv(bike_proc)
-    weather_df = pd.read_csv(weather_proc)
+    with tempfile.TemporaryDirectory() as output_file:
 
-    # bike_df['datetime'] = pd.to_datetime(bike_df['datetime'])
-    bike_df['date'] = pd.to_datetime(bike_df['datetime']).dt.date
-    bike_df['hour'] = pd.to_datetime(bike_df['datetime']).dt.hour
+        bike_df = pd.read_csv(bike_proc)
+        weather_df = pd.read_csv(weather_proc)
 
-    # weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
-    weather_df['date'] = pd.to_datetime(weather_df['datetime']).dt.date
-    weather_df['hour'] = pd.to_datetime(weather_df['datetime']).dt.hour
+        # bike_df['datetime'] = pd.to_datetime(bike_df['datetime'])
+        bike_df['date'] = pd.to_datetime(bike_df['datetime']).dt.date
+        bike_df['hour'] = pd.to_datetime(bike_df['datetime']).dt.hour
 
-    merged_data = bike_df.merge(weather_df, on=['date', 'hour'], how='inner')
+        # weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
+        weather_df['date'] = pd.to_datetime(weather_df['datetime']).dt.date
+        weather_df['hour'] = pd.to_datetime(weather_df['datetime']).dt.hour
 
-    columns = ['hour', 'vehicles_available', 'temp', 'hum', 'percp', 'wspeed', 'capacity', 'capacity_free']
-    merged_data = merged_data[columns]
+        merged_data = bike_df.merge(weather_df, on=['date', 'hour'], how='inner')
 
-    merged_data.to_csv(output_file, index=False)
+        columns = ['hour', 'vehicles_available', 'temp', 'hum', 'percp', 'wspeed', 'capacity', 'capacity_free']
+        merged_data = merged_data[columns]
+
+        merged_data.to_csv(output_file, index=False)
 
 
 if __name__ == '__main__':
