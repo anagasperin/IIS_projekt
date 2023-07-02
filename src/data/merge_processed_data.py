3a19788e5a0253705b95ea26e6ec416000acc935
anagasperin <anagasperin99@gmail.com>
2023-07-02 19:45:04 +0200

    delete temp file

--- src/data/merge_processed_data.py
+++ src/data/merge_processed_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 from datetime import datetime
-import tempfile
 
 def main():
     root_dir = os.path.abspath(os.path.join(
@@ -10,29 +9,24 @@ def main():
     bike_proc = os.path.join(root_dir, 'data', 'processed', 'bike_data.csv')
     weather_proc = os.path.join(root_dir, 'data', 'processed', 'weather_data.csv')
     output_file = os.path.join(root_dir, 'data', 'processed', 'current_data.csv')
-    os.makedirs(bike_proc, exist_ok=True)
-    os.makedirs(weather_proc, exist_ok=True)
-    os.makedirs(output_file, exist_ok=True)
 
-    with tempfile.TemporaryDirectory() as output_file:
+    bike_df = pd.read_csv(bike_proc)
+    weather_df = pd.read_csv(weather_proc)
 
-        bike_df = pd.read_csv(bike_proc)
-        weather_df = pd.read_csv(weather_proc)
+    # bike_df['datetime'] = pd.to_datetime(bike_df['datetime'])
+    bike_df['date'] = pd.to_datetime(bike_df['datetime']).dt.date
+    bike_df['hour'] = pd.to_datetime(bike_df['datetime']).dt.hour
 
-        # bike_df['datetime'] = pd.to_datetime(bike_df['datetime'])
-        bike_df['date'] = pd.to_datetime(bike_df['datetime']).dt.date
-        bike_df['hour'] = pd.to_datetime(bike_df['datetime']).dt.hour
+    # weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
+    weather_df['date'] = pd.to_datetime(weather_df['datetime']).dt.date
+    weather_df['hour'] = pd.to_datetime(weather_df['datetime']).dt.hour
 
-        # weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
-        weather_df['date'] = pd.to_datetime(weather_df['datetime']).dt.date
-        weather_df['hour'] = pd.to_datetime(weather_df['datetime']).dt.hour
+    merged_data = bike_df.merge(weather_df, on=['date', 'hour'], how='inner')
 
-        merged_data = bike_df.merge(weather_df, on=['date', 'hour'], how='inner')
+    columns = ['hour', 'vehicles_available', 'temp', 'hum', 'percp', 'wspeed', 'capacity', 'capacity_free']
+    merged_data = merged_data[columns]
 
-        columns = ['hour', 'vehicles_available', 'temp', 'hum', 'percp', 'wspeed', 'capacity', 'capacity_free']
-        merged_data = merged_data[columns]
-
-        merged_data.to_csv(output_file, index=False)
+    merged_data.to_csv(output_file, index=False)
 
 
 if __name__ == '__main__':
